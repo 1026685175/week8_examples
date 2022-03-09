55b81751ebda68f52b1a6520d91c51b7e520ddc5
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2022-03-09 12:32:14 +0000

    Remove using statements

--- sqrt/sqrt.cpp
+++ sqrt/sqrt.cpp
@@ -7,26 +7,21 @@ using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
 using std::chrono::milliseconds;
 
-using std::sqrt;
-using std::cout;
-using std::endl;
-using std::make_unique;
-
 int main() {
 
   const int N = 10000000;
-  auto array = make_unique<double[]>(N);
+  auto array = std::make_unique<double[]>(N);
 
   auto start = high_resolution_clock::now();
   
   for(int i=0; i<N; ++i) {
-    array[i] = sqrt(i);
+    array[i] = std::sqrt(i);
   }
 
   auto stop = high_resolution_clock::now();
   auto duration = duration_cast<milliseconds>(stop - start);
 
-  cout << "Time for " << N << " iterations: " << duration.count() << endl;
+  std::cout << "Time for " << N << " iterations: " << duration.count() << std::endl;
 
   return 0;
 }
